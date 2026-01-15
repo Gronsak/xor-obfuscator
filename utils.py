93c7315fe282eb8b02b9a1a9dac7db6a411ca817
Gronsak <dev@gronsak.tech>
2026-01-16 00:42:46 +0100

    Add parameter type annotation to existing functions and add a read_file function

--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
-def read_file_as_bytes(path):
+def read_file_as_bytes(path:str):
     with open(path,"rb") as file:
         data = file.read()
         return data
+
+def read_file(path:str):
+    with open(path,"r") as file:
+        data = file.read()
+        return data
     
-def write_bytes_to_file(path, data):
+def write_bytes_to_file(path:str, data:bytes):
     with open(path,"xb") as file:
         file.write(data)
 
-def write_to_file(path, data):
-    with open(path,"xb") as file:
+def write_to_file(path:str, data:str):
+    with open(path,"x") as file:
         file.write(data)
