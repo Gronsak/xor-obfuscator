63098732ea289117b72d504c3a60c8446846c58b
Gronsak <dev@gronsak.tech>
2026-01-15 15:50:21 +0100

    add a write_bytes_to_file and write_to_file functions

--- utils.py
+++ utils.py
@@ -2,3 +2,11 @@ def read_file_as_bytes(path):
     with open(path,"rb") as file:
         data = file.read()
         return data
+    
+def write_bytes_to_file(path, data):
+    with open(path,"xb") as file:
+        file.write(data)
+
+def write_to_file(path, data):
+    with open(path,"xb") as file:
+        file.write(data)
